a20bcf04466178f44814220be5321e138756c018
Ariful Haque <arifulhb@gmail.com>
2021-05-22 15:51:31 +0800

    User password encoding

--- src/main/java/com/arifulhaque/springsecuritylearning/security/ApplicationSecurityConfig.java
+++ src/main/java/com/arifulhaque/springsecuritylearning/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.arifulhaque.springsecuritylearning.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,7 +44,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         // create user
         UserDetails userAriful = User.builder()
                 .username("arifulhb")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("STUDENT") // ROLE_STUDENT
                 .build();
 
